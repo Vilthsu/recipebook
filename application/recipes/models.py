e6ea19717c65a669545be3a263882cd181ecd15f
Vilthsu <admin@localhost>
2019-02-25 16:49:50 +0200

    Raaka-aineiden tietokantakyselyt

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -38,13 +38,22 @@ class Resepti(db.Model):
 class RaakaAine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nimi = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+class ReseptiRaakaAine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    resepti_id = db.Column(db.Integer, db.ForeignKey('resepti.id'))
+    raaka_aine_id = db.Column(db.Integer, db.ForeignKey('raaka_aine.id'))
     maara = db.Column(db.Numeric(precision=3, asdecimal=False, decimal_return_scale=None), nullable=False)
     maara_yksikko_id = db.Column(db.Integer, db.ForeignKey('maara_yksikko.id'))
 
     # Vierasavaimet
+    resepti = db.relationship('Resepti', foreign_keys=resepti_id)
+    raaka_aine = db.relationship('RaakaAine', foreign_keys=raaka_aine_id)
     maarayksikko = db.relationship('MaaraYksikko', foreign_keys=maara_yksikko_id)
 
-    def __init__(self, nimi, maara, maara_yksikko_id):
-        self.nimi = nimi
+    def __init__(self, maara, maara_yksikko_id):
         self.maara = maara
         self.maara_yksikko_id = maara_yksikko_id
--- application/recipes/views.py
+++ application/recipes/views.py
@@ -45,12 +45,71 @@ def recipes_new():
     if recipe_ingredient_total <= 0 or not data[prefix + "ingredient-name[0]"]:
         return render_template("recipes/new.html", form = form, prefix = prefix, units = units, error = "Raaka-aineita tulee olla vähintään yksi")
 
+    # Luodaan raaka-aineita varten muutama apumuutuja (arrayt)
+    recipe_ingredient_name_ids = []
+    recipe_ingredient_amounts = []
+    recipe_ingredient_unit_ids = []
+
     # Käydään läpi lähetetyt raaka-aineet
     for i in range(0, recipe_ingredient_total - 1):
-        # Etsitään jo olemassa oleva raaka-aine
-        #cursor = db.engine.execute("SELECT id FROM valmistusaika WHERE tunti = ? AND minuutti = ? LIMIT 1", cooking_time_hours, cooking_time_minutes)
-        if data[prefix + "ingredient-name[" + str(i) + "]"]:
-            print("name=" + data[prefix + "ingredient-name[" + str(i) + "]"] + ",amount=" + str(data[prefix + "ingredient-amount[" + str(i) + "]"]) + ",unit=" + data[prefix + "ingredient-unit[" + str(i) + "]"])
+        # Etsitään jo olemassa olevan raaka-aineen nimi
+        if prefix + "ingredient-name[" + str(i) + "]" in data:
+            # Käsittele raaka-aineen nimi
+            ingredient_name = data[prefix + "ingredient-name[" + str(i) + "]"]
+
+            cursor = db.engine.execute("SELECT id FROM raaka_aine WHERE nimi = ? LIMIT 1", ingredient_name)
+            name_rows = cursor.fetchone()
+            cursor.close()
+
+            # Tarkistetaan onko jo olemassa olevaa ID:tä (nimen osalta)
+            if not name_rows:
+                # ID:tä ei löytynyt -> lisätään uusi raaka-aineen nimi
+                cursor = db.engine.execute("INSERT INTO raaka_aine (nimi) VALUES (?)", ingredient_name)
+                
+                # Koska lisätty rivi on viimeinen rivi, voidaan hakea sen ID suoraan vastauksesta
+                name_id = cursor.lastrowid
+                cursor.close()
+            else:
+                # Tallennetaan olemassa oleva ID muuttujaan name_id
+                name_id = name_rows.id
+
+            # Lisätään nimen ID
+            recipe_ingredient_name_ids.append(name_id)
+            
+            # Käsittele raaka-aineen määrän yksikkö, jos annettu ...
+            if data[prefix + "ingredient-unit[" + str(i) + "]"]:
+                ingredient_unit = data[prefix + "ingredient-unit[" + str(i) + "]"]
+
+                cursor = db.engine.execute("SELECT id FROM maara_yksikko WHERE nimi = ? LIMIT 1", ingredient_unit)
+                unit_rows = cursor.fetchone()
+                cursor.close()
+
+                # Tarkistetaan onko jo olemassa olevaa ID:tä (nimen osalta)
+                if not unit_rows:
+                    # ID:tä ei löytynyt -> lisätään uusi raaka-aineen nimi
+                    cursor = db.engine.execute("INSERT INTO maara_yksikko (nimi) VALUES (?)", ingredient_unit)
+                    
+                    # Koska lisätty rivi on viimeinen rivi, voidaan hakea sen ID suoraan vastauksesta
+                    unit_id = cursor.lastrowid
+                    cursor.close()
+                else:
+                    # Tallennetaan olemassa oleva ID muuttujaan name_id
+                    unit_id = unit_rows.id
+
+            else:
+                # ... muulloin ei ole yksikköä eli ID 0
+                unit_id = 0
+
+            # Lisätään yksikön ID
+            recipe_ingredient_unit_ids.append(name_id)
+
+            # Käsitellään vielä määrä
+            if data[prefix + "ingredient-amount[" + str(i) + "]"]:
+                amount = int(data[prefix + "ingredient-amount[" + str(i) + "]"])
+            else:
+                amount = 0
+            
+            recipe_ingredient_amounts.append(amount)
 
     # Etsitään jo olemassa oleva valmistusaika
     cursor = db.engine.execute("SELECT id FROM valmistusaika WHERE tunti = ? AND minuutti = ? LIMIT 1", cooking_time_hours, cooking_time_minutes)
@@ -75,11 +134,15 @@ def recipes_new():
 
     # Lisätään uusi resepti
     cursor = db.engine.execute("INSERT INTO resepti (nimi, valmistusaika_id, valmistusohje, kuvaus) VALUES (?, ?, ?, ?)", recipe_name, cooking_time_id, recipe_recipe, recipe_desc)
-    
-    # TODO: uudelleenohjaus reseptin sivulle
+
     new_recipe_id = cursor.lastrowid
     cursor.close()
+    
+    # Lisää raaka-aineiden liitostaulun data
+    for i in range(0, len(recipe_ingredient_name_ids)):
+        cursor = db.engine.execute("INSERT INTO resepti_raaka_aine (resepti_id, raaka_aine_id, maara, maara_yksikko_id) VALUES (?, ?, ?, ?)", new_recipe_id, recipe_ingredient_name_ids[i], recipe_ingredient_amounts[i], recipe_ingredient_unit_ids[i])
 
+    # TODO: uudelleenohjaus reseptin sivulle
     return redirect(url_for("recipes_list"))
 
 # Reseptisivu
